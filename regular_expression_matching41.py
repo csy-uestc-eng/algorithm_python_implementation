747afad6f172c411a6170db242a36ca5b84f0d7d
csy-uestc <645047726@qq.com>
2020-01-06 01:09:26 +0800

    fuck...don't ac

--- regular_expression_matching41.py
+++ regular_expression_matching41.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+
 class DFA:
     alphabets = [chr(p) for p in range(97, 123)]
 
@@ -34,8 +36,66 @@ class Solution_DP(object):
 
     """
     def isMatch(self, s, p):
-        len_s, len_p = len(s), len(p)
-        
+        len_s, len_p = len(s) + 1, len(p) + 1
+        match = [[False] * len_p for i in range(len_s)]
+        match[0][0] = True
+
+        # 初始化match[0][j]
+        for j in range(1, len_p):
+            if p[j - 1] != '*':
+                match[0][j] = False
+            else:
+                if j == 1:
+                    match[0][j] = True
+                else:
+                    match[0][j] = match[0][j-2]
+
+        for i in range(1, len_s):
+            for j in range(1, len_p):
+                if s[i - 1] == p[j - 1] or p[j - 1] == '.':
+                    match[i][j] = match[i - 1][j-1]
+                elif p[j - 1] == '*':
+                    if j == 1:
+                        match[i][j] = match[i][j - 1]
+                    else:
+                        if p[j - 2] == '*':
+                            # **
+                            match[i][j] = match[i][j - 1]
+                        elif p[j - 2] != '.':
+                            # [a-z]*
+                            if p[j - 2] != s[i - 1]:
+                                # abb,ac*
+                                match[i][j] = match[i][j-2]
+                            else:
+                                # abb,ab* or aa, a*
+                                for k in range(i, 0, -1):
+                                    if s[k - 1] == p[j - 2]:
+                                        # aa, ab*a*
+                                        if match[k][j-2]:
+                                            match[i][j] = match[k][j-2]
+                                            break
+                                    else:
+                                        match[i][j] = match[k][j-2] or match[i][j]
+                                        break
+                                if k == 1 and s[0] == p[j - 2] and not match[i][j]:
+                                    match[i][j] = match[0][j-2]
+                                match[i][j] = match[i][j] or match[i][j - 2]
+
+                        else:
+                            # .*
+                            if j == 2:
+                                match[i][j] = True
+                            else:
+                                # ab, ab.* or abcb, ab.*
+                                for k in range(i, 0, -1):
+                                    if s[k - 1] == p[j - 3]:
+                                        if match[k][j-2]:
+                                            match[i][j] = True
+                                            break
+                                    else:
+                                        match[i][j] = match[i][j] or match[k][j-2]
+
+        return match[len_s - 1][len_p - 1]
 
 
 class Solution(object):
--- test_regular_expression_matching41.py
+++ test_regular_expression_matching41.py
@@ -7,6 +7,18 @@ class TestSolution_DP(TestCase):
     def setUp(self):
         self.obj = Solution_DP()
 
+    def test_isMatch_special(self):
+        self.assertEqual(True, self.obj.isMatch('', '*'))
+
+    def test_isMatch_special01(self):
+        self.assertEqual(True, self.obj.isMatch('', '.*'))
+
+    def test_isMatch_special02(self):
+        self.assertEqual(False, self.obj.isMatch('', '..*'))
+
+    def test_isMatch_special03(self):
+        self.assertEqual(True, self.obj.isMatch('', '.*.*'))
+
     def test_isMatch(self):
         self.assertEqual(True, self.obj.isMatch('ab', 'ab'))
 
@@ -17,6 +29,10 @@ class TestSolution_DP(TestCase):
         # 星号0次匹配
         self.assertEqual(True, self.obj.isMatch('a', 'ab*'))
 
+    def test_isMatch_asterisk00(self):
+        # 星号1次匹配(遗漏)
+        self.assertEqual(True, self.obj.isMatch('aa', 'a*'))
+
     def test_isMatch_asterisk01(self):
         # 星号多次匹配
         self.assertEqual(True, self.obj.isMatch('abb', 'ab*'))
@@ -24,6 +40,14 @@ class TestSolution_DP(TestCase):
     def test_isMatch_asterisk02(self):
         # ** 非合法正则
         self.assertEqual(True, self.obj.isMatch('abb', 'ab**'))
+
+    def test_isMatch_asterisk_03(self):
+        # *(遗漏)
+        self.assertEqual(True, self.obj.isMatch('', 'c*'))
+
+    def test_isMatch_asterisk_04(self):
+        # *(遗漏)
+        self.assertEqual(True, self.obj.isMatch('ab', 'abc*'))
 
     def test_isMatch_comma_asterisk(self):
         # .*
@@ -35,4 +59,26 @@ class TestSolution_DP(TestCase):
 
     def test_isMatch_comma_asterisk_02(self):
         # 多个.*
-        self.assertEqual(True, self.obj.isMatch('cbab dbab cb', 'cb*.ab.*'))
+        self.assertEqual(True, self.obj.isMatch('cbab', 'cb.*ab.*'))
+
+    def test_isMatch_comma_asterisk_03(self):
+        # 多个.*
+        self.assertEqual(True, self.obj.isMatch('miss', 'mis*'))
+
+    def test_isMatch_comma_asterisk_04(self):
+        # 多个.*(遗漏)
+        self.assertEqual(True, self.obj.isMatch('aa', 'ab*a*'))
+
+    def test_isMatch_comma_asterisk_05(self):
+        # 多个.*(遗漏)
+        self.assertEqual(True, self.obj.isMatch('a', '.*.*'))
+
+    def test_isMatch_comma_asterisk_06(self):
+        # 多个.*(遗漏)
+        self.assertEqual(True, self.obj.isMatch('bab', '..*'))
+
+    def test_isMatch_comma_asterisk_07(self):
+        # 多个.*(遗漏)
+        self.assertEqual(True, self.obj.isMatch('cbbbaccbcacbcca', '.*b*a*.a*b*.a*'))
+
+
